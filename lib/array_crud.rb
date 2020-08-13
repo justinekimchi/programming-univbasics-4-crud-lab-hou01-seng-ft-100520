9c0712509c4672acd2d782e5f12258cd38f7d6d9
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-08-13 07:15:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  lab_example=["wow","I", "am","really","learning", "arrays!"]
+  lab_example[1]
 end
